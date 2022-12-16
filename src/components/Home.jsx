cfe23684b46acf2fb231c79ebfb8ff445bdd1106
Venushi Fernando <87602633+VenuFdo@users.noreply.github.com>
2022-12-17 03:30:33 +0530

    Update home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-gray-400">
-            I'm a UI/UX Designer & Full Stack Developer!
+            I'm a Content Writter & UI/UX Designer!
           </h2>
           <p className="text-yellow-500 py-4 max-w-md">
             I have two years of experience in content writting and web design.
