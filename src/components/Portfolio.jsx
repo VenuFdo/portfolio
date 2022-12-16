ab145c33933f21a029e29c6a5a2a49949b6126aa
Venushi Fernando <87602633+VenuFdo@users.noreply.github.com>
2022-12-17 03:16:08 +0530

    update pro img

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -44,7 +44,7 @@ const Portfolio = () => {
           <p className="text-4xl font-bold inline border-b-4 border-gray-500 ">
             Portfolio
           </p>
-          <p className="py-6">View some of my work by clicking here</p>
+          <p className="py-6">Here are some of my work, for more designs visit to Behance</p>
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
@@ -56,9 +56,9 @@ const Portfolio = () => {
                 className="rounded-md duration-200 hover:scale-105"
               />
               <div className="flex items-center justify-center">
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 font-bold">
+                {/* <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 font-bold">
                   Demo
-                </button>
+                </button> */}
                 {/* <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
                   Code
                 </button> */}
